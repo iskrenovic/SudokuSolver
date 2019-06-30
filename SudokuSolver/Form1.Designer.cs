b8c756c7e8c51c29099c8ec2d1db18c4ef43de59
diskren <didi.isk08@gmail.com>
2019-06-30 12:19:24 +0200

    Visual

--- SudokuSolver/Form1.Designer.cs
+++ SudokuSolver/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgv.ColumnHeadersVisible = false;
+            this.dgv.Location = new System.Drawing.Point(12, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgv.Size = new System.Drawing.Size(467, 426);
+            this.dgv.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(509, 554);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
     }
 }
 
--- SudokuSolver/Form1.cs
+++ SudokuSolver/Form1.cs
@@ -12,9 +12,52 @@ namespace SudokuSolver
 {
     public partial class Form1 : Form
     {
+        private const int fieldNumbers = 9;
+        private const int fieldSize = 50;
+        private static Color cellCollor = Color.LightGray;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitDataGridView();
+        }
+
+        private void InitDataGridView()
+        {
+            dgv.RowCount = fieldNumbers;
+            dgv.ColumnCount = fieldNumbers;
+            for(int i  = 0; i < fieldNumbers; ++i)
+            {
+                dgv.Rows[i].Height = fieldSize;                
+                dgv.Columns[i].Width = fieldSize;                
+                if (i < 3 || i > 5)
+                {
+                    dgv[i, 0].Style.BackColor = cellCollor;
+                    dgv[i, 1].Style.BackColor = cellCollor;
+                    dgv[i, 2].Style.BackColor = cellCollor;
+                    dgv[i, 6].Style.BackColor = cellCollor;
+                    dgv[i, 7].Style.BackColor = cellCollor;
+                    dgv[i, 8].Style.BackColor = cellCollor;
+                }
+                else
+                {
+                    dgv[i, 3].Style.BackColor = cellCollor;
+                    dgv[i, 4].Style.BackColor = cellCollor;
+                    dgv[i, 5].Style.BackColor = cellCollor;
+                }
+            }
+            dgv.Width = fieldSize * fieldNumbers + 1;
+            dgv.Height = fieldSize * fieldNumbers + 1;
+            dgv.AllowUserToAddRows = false;
+            dgv.AllowUserToDeleteRows = false;
+            dgv.AllowUserToOrderColumns = false;
+            dgv.AllowUserToResizeColumns = false;
+            dgv.AllowUserToResizeRows = false;            
+            
         }
     }
 }
